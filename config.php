d6b71bbd833ea9eb057fa93412dc2ce0c974358f
whiteHeX <whiteHeX@users.noreply.github.com>
2019-03-03 22:23:20 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'zz73kiY82h!Tnb7Z$v@f');
+define('DB_NAME', 'webapp');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+
