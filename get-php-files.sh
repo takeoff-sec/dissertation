0499a4c90d733ee5e4abf9d1f7396d5475573610
whiteHeX <whiteHeX@users.noreply.github.com>
2019-03-05 14:43:48 +0000

    Create get-php-files.sh

---
+++ get-php-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/whiteHeX/dissertation/master/config.php
+wget https://raw.githubusercontent.com/whiteHeX/dissertation/master/login.php
+wget https://raw.githubusercontent.com/whiteHeX/dissertation/master/logout.php
+wget https://raw.githubusercontent.com/whiteHeX/dissertation/master/register.php
+wget https://raw.githubusercontent.com/whiteHeX/dissertation/master/welcome.php
